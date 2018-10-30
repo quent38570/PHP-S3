cf52a8f4d8052cac408ac6752fede692acd542d6
unknown <ussopfr@gmail.com>
2018-10-30 14:56:35 +0100

    liste.view

--- view/liste.view.php
+++ view/liste.view.php
@@ -8,26 +8,26 @@ foreach ($variable as $key => $value) {
 
     <!-- Image du produit -->
     <div class="image">
-      <img src="http://www-etu-info.iut2.upmf-grenoble.fr/~sebags/ProgWeb/projet/view/image/IGottaFeeling.jpeg" alt="image">
+      <img src="http://www-etu-info.iut2.upmf-grenoble.fr/~sebags/ProgWeb/projet/view/image/ <?php echo $stocks['image']; ?>.jpeg" alt="image">
     </div>
     <!-- titre du produit -->
     <div class="produit">
-      <h3>I gotta feeling</h3>
+      <h3><?php echo $stocks['nomEvenement']; ?></h3>
     </div>
 
     <!-- Dans le même lieux -->
     <div class="lieux">
-      <p>super showdown</p>
+      <p><?php echo $stocks['lieu']; ?></p>
     </div>
 
     <!-- Avec le même artiste -->
     <div class="artiste">
-      <p>The Black Eyed Peas</p>
+      <p><?php echo $stocks['nbBillet']; ?> billets restants</p>
     </div>
 
     <!-- Prix Billet -->
     <div class="prix">
-      <p>20€</p>
+      <p><?php echo $stocks['prix']; ?> euros</p>
     </div>
 
 </div>
