3f398316afbc8fd81fb0314bbf79789d59d5d277
unknown <ussopfr@gmail.com>
2018-10-31 13:14:16 +0100

    yt

--- controler/zoom.ctrl.php
+++ controler/zoom.ctrl.php
@@ -2,16 +2,11 @@
 <!-- Demande à la base de données  -->
 <?php
 require_once("../model/DAO.class.php");
-var_dump($_POST['ID']);
 $dao = new BilletDAO();
-
 
 // On récupère le paramètre ID
 $idEvenement = $_POST['ID'];
-
 $stocks = ($dao -> RecupInfoProduit($idEvenement));
-
-var_dump($stocks);
 
 require_once("../view/zoom.view.php");
 ?>
