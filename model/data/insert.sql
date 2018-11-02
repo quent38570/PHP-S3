1919d1ad65a63ba721d8a26518e687af1aa91642
SEBAG Samuel <sebags@hostname.iut2.upmf-grenoble.fr>
2018-11-02 11:02:05 +0100

    bd

--- model/data/insert.sql
+++ model/data/insert.sql
@@ -1,4 +1,6 @@
 INSERT INTO utilisateur (identifiant, motDePasse) VALUES ('visiteur','visiteur');
+INSERT INTO utilisateur (identifiant, motDePasse) VALUES ('salut','salut');
+INSERT INTO utilisateur (identifiant, motDePasse) VALUES ('azerty','azerty');
 
 INSERT INTO lieux VALUES ('lieuxTest','adresseTest','www.siteTest.test');
 
@@ -7,4 +9,7 @@ INSERT INTO evenement (nomEvenement,type,libelle,image,lieu,moment,nbBillet,prix
 INSERT INTO evenement (nomEvenement,type,libelle,image,lieu,moment,nbBillet,prix) VALUES ('evnementTest3','spectacle','ceci est un test 3','test.jpg','lieuxTest3','now',12,7.0);
 INSERT INTO evenement (nomEvenement,type,libelle,image,lieu,moment,nbBillet,prix) VALUES ('evnementTest4','sport','ceci est un test 4','test.jpg','lieuxTest4','now',13,8.0);
 
-INSERT INTO panier VALUES (1,1,2);
+INSERT INTO panier VALUES (1,1,3);
+INSERT INTO panier VALUES (2,2,1);
+INSERT INTO panier VALUES (3,4,4);
+INSERT INTO panier VALUES (3,3,2);
