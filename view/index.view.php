e5c70e50df8cadd785ca741256a80e73970a87dc
SEBAG Samuel <sebags@hostname.iut2.upmf-grenoble.fr>
2018-11-02 17:24:05 +0100

    18eme commit

--- view/index.view.php
+++ view/index.view.php
@@ -3,7 +3,7 @@
 
 <!-- Dernières Minutes -->
     <div>
-      <h3>Dernières Minutes</h3>
+      <h3>Sport</h3>
       <a href="../controler/listesport.ctrl.php"><img src="http://www-etu-info.iut2.upmf-grenoble.fr/~sebags/ProgWeb/projet/view/image/IGottaFeeling.jpeg" alt="" ></img></a>
       <a href="../controler/listesport.ctrl.php"><img src="http://www-etu-info.iut2.upmf-grenoble.fr/~sebags/ProgWeb/projet/view/image/IGottaFeeling.jpeg" alt="" ></img></a>
       <a href="../controler/listesport.ctrl.php"><img src="http://www-etu-info.iut2.upmf-grenoble.fr/~sebags/ProgWeb/projet/view/image/IGottaFeeling.jpeg" alt="" ></img></a>
@@ -11,7 +11,7 @@
 
 <!--A ne pas manquer -->
     <div>
-      <h3>A ne pas manquer</h3>
+      <h3>Concert</h3>
       <a href="../controler/listeconcert.ctrl.php"><img src="http://www-etu-info.iut2.upmf-grenoble.fr/~sebags/ProgWeb/projet/view/image/IGottaFeeling.jpeg" alt="" ></img></a>
       <a href="../controler/listeconcert.ctrl.php"><img src="http://www-etu-info.iut2.upmf-grenoble.fr/~sebags/ProgWeb/projet/view/image/IGottaFeeling.jpeg" alt="" ></img></a>
       <a href="../controler/listeconcert.ctrl.php"><img src="http://www-etu-info.iut2.upmf-grenoble.fr/~sebags/ProgWeb/projet/view/image/IGottaFeeling.jpeg" alt="" ></img></a>
@@ -19,7 +19,7 @@
 
 <!--Nouveautés -->
     <div>
-      <h3>Nouveautés</h3>
+      <h3>Spectacles</h3>
       <a href="../controler/listespectacle.ctrl.php"><img src="http://www-etu-info.iut2.upmf-grenoble.fr/~sebags/ProgWeb/projet/view/image/IGottaFeeling.jpeg" alt="" ></img></a>
       <a href="../controler/listespectacle.ctrl.php"><img src="http://www-etu-info.iut2.upmf-grenoble.fr/~sebags/ProgWeb/projet/view/image/IGottaFeeling.jpeg" alt="" ></img></a>
       <a href="../controler/listespectacle.ctrl.php"><img src="http://www-etu-info.iut2.upmf-grenoble.fr/~sebags/ProgWeb/projet/view/image/IGottaFeeling.jpeg" alt="" ></img></a>
