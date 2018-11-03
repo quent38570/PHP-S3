028eed970989eb781f253858a871dd9b6c5f6001
SEBAG Samuel <sebags@hostname.iut2.upmf-grenoble.fr>
2018-11-03 21:15:45 +0100

    19eme commit

--- model/data/insert.sql
+++ model/data/insert.sql
@@ -9,22 +9,22 @@ INSERT INTO lieux VALUES ('La Belle Electrique','12 Esplanade Andry Farcy ,38000
 INSERT INTO lieux VALUES ('Summum-Alpexpo','Rue Henri Barbusse ,38100 Grenoble','http://summum-grenoble.com');
 
 INSERT INTO evenement (nomEvenement,type,libelle,image,lieu,moment,nbBillet,prix) VALUES ('LOUD','concert','Concert de LOUD à l ampérage','test.jpg','L amperage','06/12/2018',20,17.0);
-INSERT INTO evenement (nomEvenement,type,libelle,image,lieu,moment,nbBillet,prix) VALUES ('GEORGIO','concert','Concert de GEORGIO à La Belle Electrique','test.jpg','La Belle Electrique','29/03/19',30,27.0);
-INSERT INTO evenement (nomEvenement,type,libelle,image,lieu,moment,nbBillet,prix) VALUES ('KYO','concert','Concert de KYO au Summum-Alpexpo','test.jpg','Summum-Alpexpo','06/12/18',10,35.0);
+INSERT INTO evenement (nomEvenement,type,libelle,image,lieu,moment,nbBillet,prix) VALUES ('GEORGIO','concert','Concert de GEORGIO à La Belle Electrique','test.jpg','La Belle Electrique','29/03/2019',30,27.0);
+INSERT INTO evenement (nomEvenement,type,libelle,image,lieu,moment,nbBillet,prix) VALUES ('KYO','concert','Concert de KYO au Summum-Alpexpo','test.jpg','Summum-Alpexpo','06/12/2018',10,35.0);
 INSERT INTO evenement (nomEvenement,type,libelle,image,lieu,moment,nbBillet,prix) VALUES ('POSTMODERN JUKEBOX','concert','Concert de POSTMODERN JUKEBOX à La Belle Electrique','test.jpg','La Belle Electrique','06/12/2018',25,40.0);
 INSERT INTO evenement (nomEvenement,type,libelle,image,lieu,moment,nbBillet,prix) VALUES ('FLAVIEN BERGER','concert','Concert de FLAVIEN BERGER à La Belle Electrique','test.jpg','La Belle Electrique','26/01/2019',15,16.80);
 
 INSERT INTO evenement (nomEvenement,type,libelle,image,lieu,moment,nbBillet,prix) VALUES ('ARY ABITTAN','spectacle','ARY ABITTAN my story','test.jpg','Summum-Alpexpo','20/12/2018',20,34.0);
-INSERT INTO evenement (nomEvenement,type,libelle,image,lieu,moment,nbBillet,prix) VALUES ('JEFF PANACLOC','spectacle','JEFF PANACLOC contre-attaque','test.jpg','Summum-Alpexpo','03/05/19',30,43.0);
+INSERT INTO evenement (nomEvenement,type,libelle,image,lieu,moment,nbBillet,prix) VALUES ('JEFF PANACLOC','spectacle','JEFF PANACLOC contre-attaque','test.jpg','Summum-Alpexpo','03/05/2019',30,43.0);
 INSERT INTO evenement (nomEvenement,type,libelle,image,lieu,moment,nbBillet,prix) VALUES ('MALIK BENTALHA','spectacle','MALIK BENTALHA encore','test.jpg','Summum-Alpexpo','11/01/19',10,37.0);
 INSERT INTO evenement (nomEvenement,type,libelle,image,lieu,moment,nbBillet,prix) VALUES ('LAURENT GERRA','spectacle','LAURENT GERRA sans moderation','test.jpg','Summum-Alpexpo','30/11/2018',25,49.0);
 INSERT INTO evenement (nomEvenement,type,libelle,image,lieu,moment,nbBillet,prix) VALUES ('JAMEL DEBBOUZE','spectacle','Maintenant ou JAMEL','test.jpg','Summum-Alpexpo','10/04/2019',15,32.0);
 
-INSERT INTO evenement (nomEvenement,type,libelle,image,lieu,moment,nbBillet,prix) VALUES ('GRENOBLE/GAP','sport','SYNERGLACE LIGUE MAGNUS','test.jpg','Patinoire Polesud','28/12/2018',20,8.0);
-INSERT INTO evenement (nomEvenement,type,libelle,image,lieu,moment,nbBillet,prix) VALUES ('GRENOBLE/GAP','sport','SYNERGLACE LIGUE MAGNUS','test.jpg','Patinoire Polesud','18/01/19',30,8.0);
-INSERT INTO evenement (nomEvenement,type,libelle,image,lieu,moment,nbBillet,prix) VALUES ('GRENOBLE/BORDEAUX','sport','SYNERGLACE LIGUE MAGNUS','test.jpg','Patinoire Polesud','02/01/18',10,8.0);
-INSERT INTO evenement (nomEvenement,type,libelle,image,lieu,moment,nbBillet,prix) VALUES ('GRENOBLE/MONTPELLIER','sport','Match comptant pour le TOP 14','test.jpg','Stade des alpes','22/12/2018',25,24.0);
-INSERT INTO evenement (nomEvenement,type,libelle,image,lieu,moment,nbBillet,prix) VALUES ('GRENOBLE/AGEN','sport','Match comptant pour le challenge européen','test.jpg','Stade des alpes','07/12/2018',15,12.5);
+INSERT INTO evenement (nomEvenement,type,libelle,image,lieu,moment,nbBillet,prix) VALUES ('GRENOBLE Bruleurs de Loups/GAP (Hockey sur Glace)','sport','Match comptant pour la SYNERGLACE LIGUE MAGNUS','test.jpg','Patinoire Polesud','28/12/2018',20,8.0);
+INSERT INTO evenement (nomEvenement,type,libelle,image,lieu,moment,nbBillet,prix) VALUES ('GRENOBLE Bruleurs de Loups/BORDEAUX (Hockey sur Glace)','sport','Match comptant pour la SYNERGLACE LIGUE MAGNUS','test.jpg','Patinoire Polesud','02/01/2019',10,8.0);
+INSERT INTO evenement (nomEvenement,type,libelle,image,lieu,moment,nbBillet,prix) VALUES ('GRENOBLE 38/METZ (Football)','sport','Match comptant pour la Dominos Ligue 2','test.jpg','Stade des Alpes','01/12/18',10,10.0);
+INSERT INTO evenement (nomEvenement,type,libelle,image,lieu,moment,nbBillet,prix) VALUES ('FC GRENOBLE/MONTPELLIER (Rugby)','sport','Match comptant pour le TOP 14','test.jpg','Stade des alpes','22/12/2018',25,24.0);
+INSERT INTO evenement (nomEvenement,type,libelle,image,lieu,moment,nbBillet,prix) VALUES ('FC GRENOBLE/AGEN (Rugby)','sport','Match comptant pour le challenge européen','test.jpg','Stade des alpes','07/12/2018',15,12.5);
 
 INSERT INTO panier VALUES (1,1,1);
 INSERT INTO panier VALUES (2,2,1);
--- view/index.view.php
+++ view/index.view.php
@@ -3,26 +3,26 @@
 
 <!-- Dernières Minutes -->
     <div>
-      <h3>Sport</h3>
-      <a href="../controler/listesport.ctrl.php"><img src="http://www-etu-info.iut2.upmf-grenoble.fr/~sebags/ProgWeb/projet/view/image/IGottaFeeling.jpeg" alt="" ></img></a>
-      <a href="../controler/listesport.ctrl.php"><img src="http://www-etu-info.iut2.upmf-grenoble.fr/~sebags/ProgWeb/projet/view/image/IGottaFeeling.jpeg" alt="" ></img></a>
-      <a href="../controler/listesport.ctrl.php"><img src="http://www-etu-info.iut2.upmf-grenoble.fr/~sebags/ProgWeb/projet/view/image/IGottaFeeling.jpeg" alt="" ></img></a>
-    </div>
-
-<!--A ne pas manquer -->
-    <div>
       <h3>Concert</h3>
-      <a href="../controler/listeconcert.ctrl.php"><img src="http://www-etu-info.iut2.upmf-grenoble.fr/~sebags/ProgWeb/projet/view/image/IGottaFeeling.jpeg" alt="" ></img></a>
-      <a href="../controler/listeconcert.ctrl.php"><img src="http://www-etu-info.iut2.upmf-grenoble.fr/~sebags/ProgWeb/projet/view/image/IGottaFeeling.jpeg" alt="" ></img></a>
-      <a href="../controler/listeconcert.ctrl.php"><img src="http://www-etu-info.iut2.upmf-grenoble.fr/~sebags/ProgWeb/projet/view/image/IGottaFeeling.jpeg" alt="" ></img></a>
+      <a href="../controler/listeconcert.ctrl.php"><img src="../view/image/IGottaFeeling.jpeg" alt="image" ></img></a>
+      <a href="../controler/listeconcert.ctrl.php"><img src="../view/image/IGottaFeeling.jpeg" alt="image" ></img></a>
+      <a href="../controler/listeconcert.ctrl.php"><img src="../view/image/IGottaFeeling.jpeg" alt="image" ></img></a>
     </div>
 
-<!--Nouveautés -->
+<!--Spectacles -->
     <div>
-      <h3>Spectacles</h3>
-      <a href="../controler/listespectacle.ctrl.php"><img src="http://www-etu-info.iut2.upmf-grenoble.fr/~sebags/ProgWeb/projet/view/image/IGottaFeeling.jpeg" alt="" ></img></a>
-      <a href="../controler/listespectacle.ctrl.php"><img src="http://www-etu-info.iut2.upmf-grenoble.fr/~sebags/ProgWeb/projet/view/image/IGottaFeeling.jpeg" alt="" ></img></a>
-      <a href="../controler/listespectacle.ctrl.php"><img src="http://www-etu-info.iut2.upmf-grenoble.fr/~sebags/ProgWeb/projet/view/image/IGottaFeeling.jpeg" alt="" ></img></a>
+      <h3>Spectacle</h3>
+      <a href="../controler/listespectacle.ctrl.php"><img src="../view/image/IGottaFeeling.jpeg" alt="image" ></img></a>
+      <a href="../controler/listespectacle.ctrl.php"><img src="../view/image/IGottaFeeling.jpeg" alt="image" ></img></a>
+      <a href="../controler/listespectacle.ctrl.php"><img src="../view/image/IGottaFeeling.jpeg" alt="image" ></img></a>
+    </div>
+
+<!--Sports -->
+    <div>
+      <h3>Sport</h3>
+      <a href="../controler/listesport.ctrl.php"><img src="../view/image/bruleursdeloupslogo.jpeg" alt="image" ></img></a>
+      <a href="../controler/listesport.ctrl.php"><img src="../view/image/fcgrenoble.png" alt="image" ></img></a>
+      <a href="../controler/listesport.ctrl.php"><img src="../view/image/gf38.png" alt="image" ></img></a>
     </div>
 
 <?php include 'footer.view.html'; ?>
